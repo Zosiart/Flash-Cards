7565b92924f5319caa7d51b5b84d47bb8e9cb773
Zofia Rogacka-Trojak <zosiart@gmail.com>
2023-06-01 17:19:20 +0200

    Final version

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# Flash Cards
+# Dutch Flash Cards
+## Description
+Simple Flash Card application for learning Dutch! Progress is saved to a database so you can pick up where you left off!
+## How to use
+1. Clone the repository
+2. Run `main.py`
+3. Learn Dutch!
+4. To add new words, edit the `dutch_words.csv` file
+5. To reset your progress, delete the `words_to_learn.csv` file
+
+## Screenshots
+<img src="images/ss2.png" width="450">
+<img src="images/ss1.png" width="450">
+
+Created as a part of Udemys 100 days of code course by Angela Yu.
---
+++ main.py
@@ -0,0 +1,72 @@
+from tkinter import *
+import pandas
+import random
+
+BACKGROUND_COLOR = "#B1DDC6"
+current_card = {}
+to_learn = {}
+
+# ---------------------------- READ DATA AND PREPARE FLASH CARDS ------------------------------- #
+try:
+    short = pandas.read_csv("./data/words_to_learn.csv.csv")
+except FileNotFoundError:
+    everything = pandas.read_csv("./data/dutch_words.csv")
+    data = everything.loc[:, ["Dutch", "English"]]
+    to_learn = data.to_dict(orient="records")
+else:
+    to_learn = short.to_dict(orient="records")
+
+# ---------------------------- NEXT FLASH CARD GENERATOR ------------------------------- #
+def next_card():
+    global current_card, flip_timer
+    window.after_cancel(flip_timer)
+    current_card = random.choice(to_learn)
+    canvas.itemconfig(card_title, text="Dutch", fill="black")
+    canvas.itemconfig(card_word, text=current_card["Dutch"], fill="black")
+    canvas.itemconfig(card_background, image=card_front_img)
+    flip_timer = window.after(3000, func=flip_card)
+
+def flip_card():
+    canvas.itemconfig(card_title, text="English", fill="white")
+    canvas.itemconfig(card_word, text=current_card["English"], fill="white")
+    canvas.itemconfig(card_background, image=card_back_img)
+
+# ---------------------------- KNOWN ------------------------------- #
+def known():
+    to_learn.remove(current_card)
+    data = pandas.DataFrame(to_learn)
+    data.to_csv("./data/words_to_learn.csv", index=False)
+    next_card()
+
+# ---------------------------- UI SETUP ------------------------------- #
+window = Tk()
+window.title("Flashy")
+window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
+
+flip_timer = window.after(3000, func=flip_card)
+
+# Canvas
+canvas = Canvas(width=800, height=526, bg=BACKGROUND_COLOR, highlightthickness=0)
+card_front_img = PhotoImage(file="./images/card_front.png")
+card_back_img = PhotoImage(file="./images/card_back.png")
+card_background = canvas.create_image(400, 263, image=card_front_img)
+canvas.grid(row=0, column=0, columnspan=2)
+
+# Buttons
+wrong_img = PhotoImage(file="./images/wrong.png")
+wrong_button = Button(image=wrong_img, highlightthickness=0, command=next_card)
+wrong_button.grid(row=1, column=0)
+
+right_img = PhotoImage(file="./images/right.png")
+right_button = Button(image=right_img, highlightthickness=0, command=known)
+right_button.grid(row=1, column=1)
+
+# Labels
+card_title = canvas.create_text(400, 150, text="", font=("Arial", 40, "italic"))
+card_word = canvas.create_text(400, 263, text="", font=("Arial", 60, "bold"))
+
+
+next_card()
+
+window.mainloop()
+
